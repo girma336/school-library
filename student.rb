6ff2fd78fe2c5c5a66b452d25d29c9f4538055aa
girma <girmatarekegn.gi@gmail.com>
2023-02-13 15:03:42 +0300

    Add student and teacher class

---
+++ student.rb
@@ -0,0 +1,16 @@
+require './person'
+
+class Student < Person
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+# person2 = Student.new('A', 8, 'girma', parent_permission: false)
+
+# puts person2.play_hooky
